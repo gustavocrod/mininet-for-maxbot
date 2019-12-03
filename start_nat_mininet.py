bfbf7683a4062c8f8b3f3856829376835e7beeed
Gustavo C. Rodrigues <37716771+gustavocrod@users.noreply.github.com>
2019-12-03 16:01:46 -0300

    Update start_nat_mininet.py

--- start_nat_mininet.py
+++ start_nat_mininet.py
@@ -5,17 +5,16 @@ from mininet.log import setLogLevel
 from mininet.link import Link, TCLink
 from mininet.nodelib import NAT
 from mininet.topo import Topo
-
-NAT_IP = '10.0.3.2'
             
 class MiniTopo():
-      def build(self):  
+      def build(self, NAT_IP='10.0.3.2'):  
 
         ##### inicializacao dos hots #####
         
         client1 = self.addHost( 'cliente01', ip='10.0.1.2', defaultRoute='via ' + NAT_IP )   #host cliente chatBot
         client2 = self.addHost( 'cliente02', ip='10.0.2.2', defaultRoute='via ' + NAT_IP )   #host servidor chatBot
         client3 = self.addNode( 'cliente03', cls=NAT, ip=NAT_IP, inNamespace=False ) # configuracao da nat
+ 
         ##### Configracao dos switchs (roteadores) #####
         router1 = self.addSwitch( 'roteador1' )
         router2 = self.addSwitch( 'roteador2' )
@@ -33,7 +32,7 @@ class MiniTopo():
         
 def start_topology():
         topo = MiniTopo()
-        net = Mininet(topo=topo)
+        net = Mininet(topo)
         
         net.start()
 
