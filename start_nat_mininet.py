fd4defcdc7973bbddcf25b86f5af4283eaf8a483
Gustavo C. Rodrigues <37716771+gustavocrod@users.noreply.github.com>
2019-12-03 15:52:09 -0300

    Update and rename start_nat_mininet to start_nat_mininet.py

---
+++ start_nat_mininet.py
@@ -0,0 +1,46 @@
+from mininet.net import Mininet
+from mininet.node import Controller, RemoteController, OVSKernelSwitch, UserSwitch
+from mininet.cli import CLI
+from mininet.log import setLogLevel
+from mininet.link import Link, TCLink
+
+NAT_IP = '10.0.3.2'
+            
+class MiniTopo():
+      def build(self):  
+
+        ##### inicializacao dos hots #####
+        
+        cliente01 = self.addHost( 'cliente01', ip='10.0.1.2', defaultRoute='via ' + NAT_IP )   #host cliente chatBot
+        cliente02 = self.addHost( 'cliente02', ip='10.0.2.2', defaultRoute='via ' + NAT_IP )   #host servidor chatBot
+        cliente03 = self.addNode( 'cliente03', cls=NAT, ip=NAT_IP, inNamespace=False ) # configuracao da nat
+        ##### Configracao dos switchs (roteadores) #####
+        roteador1 = self.addSwitch( 'roteador1' )
+        roteador2 = self.addSwitch( 'roteador2' )
+        roteador3 = self.addSwitch( 'roteador3' )
+
+        # Adiciona enlaces entre os roteadores
+        self.addLink( router1, router2 )
+        self.addLink( router2, router3 )
+
+        #Adiciona enlaces entre hosts e roteadores
+        self.addLink( client1, router1 )
+        self.addLink( client2, router2 )
+        self.addLink( client3, router3 )
+
+        
+def start_topology():
+        topo = MiniTopo()
+        net = Mininet(topo=topo)
+        
+        net.start()
+
+        print "*** Rede Iniciada ***"
+        CLI( NET )      # inicia rede com todas as configuracoes setadas
+
+        print "*** Rede finalizada ***"
+        net.stop()      # finaliza rede
+
+if __name__ == '__main__':
+    setLogLevel( 'info' )
+    start_topology() 
