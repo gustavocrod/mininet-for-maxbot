7175cf3bc0c7b22db2e8f21b795134da854bc9be
Gustavo C. Rodrigues <37716771+gustavocrod@users.noreply.github.com>
2019-12-03 16:50:09 -0300

    Update start_nat_mininet.py

--- start_nat_mininet.py
+++ start_nat_mininet.py
@@ -1,5 +1,4 @@
 from mininet.net import Mininet
-from mininet.node import Controller, RemoteController, OVSKernelSwitch, UserSwitch
 from mininet.cli import CLI
 from mininet.log import setLogLevel
 from mininet.link import Link, TCLink
@@ -8,13 +7,13 @@ from mininet.topo import Topo
 from mininet.util import dumpNodeConnections
             
 class MiniTopo(Topo):
-      def build(self, NAT_IP='10.0.3.2'):  
+      def build(self, NAT_IP='10.0.0.5'):  
 
         ##### inicializacao dos hots #####
         
-        client1 = self.addHost( 'cliente01', defaultRoute='via ' + NAT_IP )   #host cliente chatBot
-        client2 = self.addHost( 'cliente02', defaultRoute='via ' + NAT_IP )   #host servidor chatBot
-        client3 = self.addNode( 'cliente03', cls=NAT, ip=NAT_IP + '/24', inNamespace=False ) # configuracao da nat
+        client1 = self.addHost( 'cliente1', ip='10.0.0.1/24', defaultRoute='via ' + NAT_IP )   # host cliente chatBot
+        client2 = self.addHost( 'cliente2', ip='10.0.0.2/24', defaultRoute='via ' + NAT_IP )   # host servidor chatBot
+        client3 = self.addNode( 'cliente3', cls=NAT, ip=NAT_IP + '/24', inNamespace=False ) # configuracao da nat
  
         ##### Configracao dos switchs (roteadores) #####
         router1 = self.addSwitch( 'roteador1' )
